a0d90583a6ad137a3e9dca3f0880a72aa09b478b
"remixwithkj" <"kumarjay3647@gmail.com">
2022-03-27 11:42:56 +0300

    soliditywithThomasW

---
+++ var.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.5.13;
+
+contract Workingwithvar {
+    string public myString = 'hello world';
+    uint256 public myUint;
+
+    function setMyUint(uint _myUint) public {
+        myUint = _myUint;
+    }
+    
+    bool public myBool; 
+
+    function setMyBool(bool _myBool) public {
+        myBool=_myBool;
+    }
+
+    uint8 public myUint8;
+
+    function incrementUint() public {
+        myUint8++;
+    }
+
+    function decrementUint() public {
+        myUint8--;
+    }
+    
+    address public myAddress;
+
+    function setAddress(address _address) public {
+        myAddress = _address;
+    }
+
+    function getBalanceOfAddress() public view returns(uint) {
+        return myAddress.balance;
+    }
+
+    
+}
